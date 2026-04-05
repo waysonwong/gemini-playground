fadfe703a8bc77741f843465f1ceacf7bb8e0cdc
waysonwong <waysonwong@users.noreply.github.com>
2026-04-06 05:17:47 +0800

    Update config settings

--- src/static/js/config/config.js
+++ src/static/js/config/config.js
@@ -3,8 +3,7 @@ export const CONFIG = {
         //VERSION: 'v1',
         VERSION: 'v1beta',
         //VERSION: 'v1alpha',
-        MODEL_NAME: 'models/gemini-2.5-flash'
-        //MODEL_NAME: 'models/gemini-live-2.5-flash-preview'
+        MODEL_NAME: 'models/gemini-live-2.5-flash-preview'
         //MODEL_NAME: 'models/gemini-3-flash-preview'
     },
     // You can change the system instruction to your liking
